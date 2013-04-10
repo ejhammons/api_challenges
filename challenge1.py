8d48ebe82e3d97bdfa45a3c20dfb30336591d77c
ejhammons <jason.hammons@rackspace.com>
2013-04-09 22:21:52 -0500

    Changed to use a non-destructive get() for all servers

--- challenge1.py
+++ challenge1.py
@@ -23,29 +23,28 @@ server1 = cs.servers.create(server1_name, image.id, flavor.id)
 server2 = cs.servers.create(server2_name, image.id, flavor.id)
 server3 = cs.servers.create(server3_name, image.id, flavor.id)
 
-server1_pw = server1.adminPass
-server2_pw = server2.adminPass
-server3_pw = server3.adminPass
-
 while ( (server1.status != "ACTIVE") or (server2.status != "ACTIVE") or (server3.status != "ACTIVE") ):
     print "Server status: ", server1.status, "|", server2.status, "|", server3.status
     time.sleep(10)
-    server1 = cs.servers.get(server1.id)
-    server2 = cs.servers.get(server2.id)
-    server3 = cs.servers.get(server3.id)
+    server1.get()
+    server2.get()
+    server3.get()
+
+
+print "Server status: ", server1.status, "|", server2.status, "|", server3.status
 
 print "Server name: ", server1.name
 print "Server IP: ", str(server1.accessIPv4)
-print "Server root password: ", server1_pw
+print "Server root password: ", server1.adminPass
 
 print
 
 print "Server name: ", server2.name
 print "Server IPs: ", str(server2.accessIPv4)
-print "server root password: ", server2_pw
+print "server root password: ", server2.adminPass
 
 print
 
 print "Server name: ", server3.name
 print "Server IPs: ", str(server3.accessIPv4)
-print "server root password: ", server3_pw
+print "server root password: ", server3.adminPass
